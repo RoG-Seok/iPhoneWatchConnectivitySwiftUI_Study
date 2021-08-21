63635c6a0e71aaec4bfaf805f9c181bfa21d523f
devboogie <goboogie@icloud.com>
2021-08-22 01:56:26 +0900

    게이지 값 전달

--- ConnectivitySwiftUI WatchKit Extension/LabeledGauge.swift
+++ ConnectivitySwiftUI WatchKit Extension/LabeledGauge.swift
@@ -10,14 +10,14 @@ import Foundation
 import SwiftUI
 
 struct LabeledGauge: View {
-    @State var current = 85.0
+    @Binding var current :Double
     @State private var minValue = 0.0
     @State private var maxValue = 170.0
     let gradient = Gradient(colors: [.green, .yellow, .orange, .red])
 
     var body: some View {
         if #available(watchOSApplicationExtension 7.0, *) {
-            Gauge(value: current, in: minValue...maxValue) {
+            Gauge(value: current , in: minValue...maxValue) {
                 Text("BPM")
             } currentValueLabel: {
                 Text("\(Int(current))")
@@ -32,5 +32,15 @@ struct LabeledGauge: View {
         }
         //.gaugeStyle(.circular)
        // .gaugeStyle(.Body)
+    }
+}
+
+
+struct LabeledGauge_Previews: PreviewProvider {
+    @ObservedObject static  var model = ViewModelWatch()
+    // 디지털크라운 회전 수치
+    @State static var scrollAmonut = 0.0 //current
+    static var previews: some View {
+        LabeledGauge(current: $scrollAmonut)
     }
 }
